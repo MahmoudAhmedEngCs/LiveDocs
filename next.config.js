330de12b0cd0f1cf55c963218312967bae5bf573
MahmoudAhmedEngCs <mahmoud.ahmed.cs.eng@gmail.com>
2025-04-11 14:53:37 +0200

    Update and rename next.config.mjs to next.config.js

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const { withSentryConfig } = require('@sentry/nextjs');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }]
+  }
+};
+
+module.exports = withSentryConfig(nextConfig, {
+  org: "mm-rq",
+  project: "javascript-nextjs",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  reactComponentAnnotation: {
+    enabled: true,
+  },
+  disableLogger: true,
+  automaticVercelMonitors: true,
+});
