efdf18a814115f5fa3941db79697c14b90db67d2
MahmoudAhmedEngCs <mahmoud.ahmed.cs.eng@gmail.com>
2025-04-11 14:49:28 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,13 @@
 import {withSentryConfig} from '@sentry/nextjs';
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-   typescript: {
+const nextConfig = {
+  typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    remotePatterns: [{protocol: 'https',hostname:'img.clerk.com'}],  
-  },
- 
+    remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }]
+  }
 };
 
 export default withSentryConfig(nextConfig, {
